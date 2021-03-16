b1426874a86465576011bf26949fa5bc4f0400a3
farepule <faruk.puska@gmail.com>
2021-03-16 21:54:41 +0100

    boolean

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+function myBoolean(myState) {
+	if(myState){
+		return "true";
+	}else{
+	return "false";
+}
+}
+console.log(myBoolean(true));
+console.log(myBoolean(false));
+myBoolean(true);
+myBoolean(false);
+
+
+// Only change code above this line
+module.exports = myFunction;
