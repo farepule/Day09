38577981dde7566f800dcf522078ad85c26647a7
farepule <faruk.puska@gmail.com>
2021-03-17 08:43:39 +0100

    switchCase

---
+++ ex11/src/index.js
@@ -0,0 +1,29 @@
+// Only change code below this line
+function switchCase(letter) {
+    var animals;
+    
+    switch (letter){
+        case "a":
+            animals = "antilope";
+            break;
+        case "b":
+            animals = "bird";
+            break;
+        case "c":
+            animals = "cat";
+            break;
+        default:
+            animals = "stuff";
+            break;
+
+
+    }
+    return animals;
+}
+console.log(switchCase("a"));
+console.log(switchCase("b"));
+console.log(switchCase("c"));
+console.log(switchCase("d"));
+console.log(switchCase(4));
+//Only change code above this line
+module.exports = switchCase;
